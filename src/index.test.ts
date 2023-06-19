869a902d512f6b130d20bfab49f565b7d640794a
Petra Jaros <peeja@peeja.com>
2023-06-19 12:18:29 -0400

    Scaffold codebase

---
+++ src/index.test.ts
@@ -0,0 +1,8 @@
+import value from "./index";
+import { describe, it, expect } from "@jest/globals";
+
+describe("the value", () => {
+  it("should be 2", () => {
+    expect(value).toBe(2);
+  });
+});
