0a7000c27d700647e538c95892c8975e6292072e
Petra Jaros <peeja@peeja.com>
2023-08-09 10:40:11 -0400

    Move `parseEntry()` up to module level

--- src/parse/parseNodeObject.ts
+++ src/parse/parseNodeObject.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-throw-literal */
 import { Map } from "immutable";
-import jsonld from "jsonld";
+import jsonld, { type ActiveContext } from "jsonld";
 import Context from "jsonld/lib/context";
 import {
   isObject,
@@ -9,14 +9,8 @@ import {
   isNumber,
   isBoolean,
 } from "lodash-es";
-import {
-  mapParallelAsync,
-  reduce,
-  toPairs,
-  concat,
-  filter,
-  piped,
-} from "rambdax";
+import { mapParallelAsync, reduce, toPairs, concat, filter } from "rambdax";
+import { type NamedNode } from "rdf-data-factory";
 
 import { isArray, nestWarningsUnderKey, type Parsed } from "./common";
 import { parsePlural } from "./parsePlural";
@@ -24,6 +18,7 @@ import * as IR from "../IntermediateResult";
 import { af, df, PLACEHOLDER } from "../common";
 import { toRdfLiteral } from "../representation";
 import { evolve, keys, pipedAsync, anyPass } from "../upstream/rambda";
+import { partial } from "../upstream/rambda/partial";
 import { variableUnder } from "../variableUnder";
 
 import type * as RDF from "@rdfjs/types";
@@ -44,25 +39,30 @@ const predicateForKey = (key: string, ctx: jsonld.ActiveContext) => {
 
 const isPlaceholder = (v: unknown): v is typeof PLACEHOLDER =>
   v === PLACEHOLDER;
+
 const isLiteral = anyPass([isString, isNumber, isBoolean]);
 
 const addMapping =
   (k: string, v: IR.IntermediateResult) => (ir: IR.NodeObject) =>
     ir.addMapping(k, v);
 
+const isId = (ctx: ActiveContext, k: string) =>
+  Context.expandIri(ctx, k, { vocab: true }) === "@id";
+
+// TODO:
+// - "parent" doesn't seem like the right name
+
 export const parseNodeObject = async (
   query: jsonld.NodeObject,
   parent: RDF.Variable,
-  outerCtx: Context.ActiveContext
+  outerCtx: ActiveContext
 ): Promise<Parsed<IR.NodeObject>> => {
   const ctx = query["@context"]
     ? await jsonld.processContext(outerCtx, query["@context"])
     : outerCtx;
 
-  const isId = (k: string) =>
-    Context.expandIri(ctx, k, { vocab: true }) === "@id";
+  const [idKey, ...extraIdKeys] = filter(partial(isId, ctx), keys(query));
 
-  const [idKey, ...extraIdKeys] = piped(keys(query), filter(isId));
   if (extraIdKeys.length)
     throw "TODO: Invalid JSON-LD syntax; colliding keywords detected.";
   const id = idKey && query[idKey];
@@ -81,7 +81,7 @@ export const parseNodeObject = async (
     value: unknown
   ]): Promise<(p: Parsed<IR.NodeObject>) => Parsed<IR.NodeObject>> => {
     const { intermediateResult, patterns, projections, warnings } =
-      await parseEntry([key, value]);
+      await parseEntry([key, value], parent, ctx, node);
 
     return evolve({
       intermediateResult: addMapping(key, intermediateResult),
@@ -91,107 +91,109 @@ export const parseNodeObject = async (
     });
   };
 
-  const parseEntry = async ([key, value]: [
-    key: string,
-    value: unknown
-  ]): Promise<Parsed<IR.IntermediateResult>> => {
-    if (key === "@context") {
-      return {
-        // TODO: Remove type assertion
-        intermediateResult: new IR.NativeValue(value as JsonValue),
-        patterns: [],
-        projections: [],
-        warnings: [],
-      };
-    }
-
-    if (isId(key)) {
-      if (!isString(value)) throw "TODO: Name must be a string";
-      return {
-        intermediateResult: new IR.Name(df.namedNode(value)),
-        patterns: [],
-        projections: [],
-        warnings: [],
-      };
-    }
-
-    const predicate = predicateForKey(key, ctx);
-
-    if (!predicate) {
-      // Key is not defined in the context.
-      return {
-        // TODO: Remove type assertion
-        intermediateResult: new IR.NativeValue(value as JsonValue),
-        patterns: [],
-        projections: [],
-        warnings: [
-          {
-            message: "Placeholder ignored at key not defined by context",
-            path: [],
-          },
-        ],
-      };
-    }
-
-    if (isPlaceholder(value)) {
-      const variable = variableUnder(parent, key);
-
-      return {
-        intermediateResult: new IR.NativePlaceholder(variable),
-        patterns: [af.createPattern(node, predicate, variable)],
-        projections: [variable],
-        warnings: [],
-      };
-    } else if (isLiteral(value)) {
-      return {
-        intermediateResult: new IR.NativeValue(value),
-        patterns: [af.createPattern(node, predicate, toRdfLiteral(value))],
-        projections: [],
-        warnings: [],
-      };
-    } else if (isArray(value)) {
-      const variable = variableUnder(parent, key);
-      // TODO: Remove type assertion
-      const parsedChild = await parsePlural(
-        value as jsonld.NodeObject[],
-        variable,
-        ctx
-      );
-      return {
-        intermediateResult: parsedChild.intermediateResult,
-        patterns: [
-          af.createPattern(node, predicate, variable),
-          ...parsedChild.patterns,
-        ],
-        projections: parsedChild.projections,
-        warnings: parsedChild.warnings,
-      };
-    } else if (isObject(value)) {
-      const variable = variableUnder(parent, key);
-      // TODO: Remove type assertion
-      const parsedChild = await parseNodeObject(
-        value as jsonld.NodeObject,
-        variable,
-        ctx
-      );
-      return {
-        intermediateResult: parsedChild.intermediateResult,
-        patterns: [
-          af.createPattern(node, predicate, variable),
-          ...parsedChild.patterns,
-        ],
-        projections: parsedChild.projections,
-        warnings: [],
-      };
-    } else {
-      throw "TODO: Not yet covered";
-    }
-  };
-
   return pipedAsync(
     query,
     toPairs,
     mapParallelAsync(operationForEntry),
     reduce((acc, f) => f(acc), init)
   );
+};
+
+const parseEntry = async (
+  [key, value]: [key: string, value: unknown],
+  parent: RDF.Variable,
+  ctx: ActiveContext,
+  node: RDF.Variable | NamedNode
+): Promise<Parsed<IR.IntermediateResult>> => {
+  if (key === "@context") {
+    return {
+      // TODO: Remove type assertion
+      intermediateResult: new IR.NativeValue(value as JsonValue),
+      patterns: [],
+      projections: [],
+      warnings: [],
+    };
+  }
+
+  if (isId(ctx, key)) {
+    if (!isString(value)) throw "TODO: Name must be a string";
+    return {
+      intermediateResult: new IR.Name(df.namedNode(value)),
+      patterns: [],
+      projections: [],
+      warnings: [],
+    };
+  }
+
+  const predicate = predicateForKey(key, ctx);
+
+  if (!predicate) {
+    // Key is not defined in the context.
+    return {
+      // TODO: Remove type assertion
+      intermediateResult: new IR.NativeValue(value as JsonValue),
+      patterns: [],
+      projections: [],
+      warnings: [
+        {
+          message: "Placeholder ignored at key not defined by context",
+          path: [],
+        },
+      ],
+    };
+  }
+
+  if (isPlaceholder(value)) {
+    const variable = variableUnder(parent, key);
+
+    return {
+      intermediateResult: new IR.NativePlaceholder(variable),
+      patterns: [af.createPattern(node, predicate, variable)],
+      projections: [variable],
+      warnings: [],
+    };
+  } else if (isLiteral(value)) {
+    return {
+      intermediateResult: new IR.NativeValue(value),
+      patterns: [af.createPattern(node, predicate, toRdfLiteral(value))],
+      projections: [],
+      warnings: [],
+    };
+  } else if (isArray(value)) {
+    const variable = variableUnder(parent, key);
+    // TODO: Remove type assertion
+    const parsedChild = await parsePlural(
+      value as jsonld.NodeObject[],
+      variable,
+      ctx
+    );
+    return {
+      intermediateResult: parsedChild.intermediateResult,
+      patterns: [
+        af.createPattern(node, predicate, variable),
+        ...parsedChild.patterns,
+      ],
+      projections: parsedChild.projections,
+      warnings: parsedChild.warnings,
+    };
+  } else if (isObject(value)) {
+    const variable = variableUnder(parent, key);
+    // TODO: Remove type assertion
+    const parsedChild = await parseNodeObject(
+      value as jsonld.NodeObject,
+      variable,
+      ctx
+    );
+    return {
+      intermediateResult: parsedChild.intermediateResult,
+      patterns: [
+        af.createPattern(node, predicate, variable),
+        ...parsedChild.patterns,
+      ],
+      projections: parsedChild.projections,
+      warnings: [],
+    };
+  } else {
+    throw "TODO: Not yet covered";
+  }
 };
