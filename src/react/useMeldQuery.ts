c832c0a55b45f7a7cef55aeff12d9662a70f1d5c
Petra Jaros <peeja@peeja.com>
2024-07-07 14:16:28 -0400

    Extract `observeMeldQuery`

--- src/react/useMeldQuery.ts
+++ src/react/useMeldQuery.ts
@@ -1,9 +1,26 @@
 import { useEffect, useState } from "react";
+import { Observable } from "rxjs";
 
 import { type ReadQueryResult, readQuery } from "..";
 
 import type { MeldClone, MeldReadState } from "@m-ld/m-ld";
 import type { JsonValue } from "type-fest";
+
+const observeMeldQuery = <Data>(meld: MeldClone, query: JsonValue) => {
+  return new Observable<ReadQueryResult<Data>>((subscriber) => {
+    const doRead = async (state: MeldReadState) => {
+      return readQuery(state, query).then((results) => {
+        // TODO: Replace this type assertion with actually derived
+        // types.
+        subscriber.next(results as ReadQueryResult<Data>);
+      });
+    };
+    const subscription = meld.read(doRead, (_update, state) => doRead(state));
+    return () => {
+      subscription.unsubscribe();
+    };
+  });
+};
 
 // Use `Data` to explicitly assert the shape of the returned data.
 export const useMeldQuery = <Data>(
@@ -14,15 +31,8 @@ export const useMeldQuery = <Data>(
 
   useEffect(() => {
     if (meld) {
-      const doRead = async (state: MeldReadState) => {
-        return readQuery(state, query).then((results) => {
-          // TODO: Replace this type assertion with actually derived
-          // types.
-          setResult(results as ReadQueryResult<Data>);
-        });
-      };
-
-      const subscription = meld.read(doRead, (_update, state) => doRead(state));
+      const o = observeMeldQuery<Data>(meld, query);
+      const subscription = o.subscribe(setResult);
 
       return () => {
         subscription.unsubscribe();
