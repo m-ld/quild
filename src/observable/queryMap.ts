8b27d443fd688f9cbc890ca00af5d967a47b566d
Petra Jaros <peeja@peeja.com>
2024-07-08 17:02:58 -0400

    Correct usage of "operator"
    https://rxjs.dev/guide/glossary-and-semantics#operator

--- src/observable/queryMap.ts
+++ src/observable/queryMap.ts
@@ -6,14 +6,15 @@ import type * as RDF from "@rdfjs/types";
 import type { JsonValue } from "type-fest";
 
 /**
- * Maps an Observable of RDF sources (taken as sequential states of a mutable
- * store) to the result of a Quild query over each.
+ * An operator which maps an Observable of RDF sources (taken as sequential
+ * states of a mutable store) to an Observable of the result of a Quild query
+ * over each.
  *
  * @template Data The expected shape of the data returned by the query.
  *                Eventually, this will be derived from the query itself. For
  *                now, it must be given explicitly.
  * @param query The Quild query to run.
- * @returns An operator that maps RDF sources to query results.
+ * @returns An operator function that maps RDF sources to query results.
  */
 export const queryMap = <Data extends JsonValue>(
   query: JsonValue
