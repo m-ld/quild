88ade55cb08a3775e63259082e1dbd2d75eeae39
Petra Jaros <peeja@peeja.com>
2024-07-11 13:08:03 -0400

    JSDoc for `NamePlaceholder` and `NativePlaceholder`

--- src/IntermediateResult/NamePlaceholder.ts
+++ src/IntermediateResult/NamePlaceholder.ts
@@ -5,6 +5,24 @@ import type { IntermediateResult } from "./types";
 import type * as RDF from "@rdfjs/types";
 import type { JsonValue } from "type-fest";
 
+/**
+ * Represents a placeholder which expects a JSON-LD/RDF name. When a solution is
+ * given binding its variable to an {@link RDF.NamedNode}, it will resolve to
+ * the node's name. This distinguishes it from {@link NativePlaceholder}, which
+ * represents a native scalar. A string in {@link NamePlaceholder}'s result
+ * represents the name of an RDF node, *not* an RDF string value.
+ *
+ * @example
+ * ### Query
+ * ```json
+ * { "@id": "?" }
+ * ```
+ *
+ * ### Result
+ * ```json
+ * { "@id": "https://swapi.dev/api/people/1/" }
+ * ```
+ */
 export class NamePlaceholder implements IntermediateResult {
   constructor(private readonly variable: RDF.Variable) {}
 
--- src/IntermediateResult/NativePlaceholder.ts
+++ src/IntermediateResult/NativePlaceholder.ts
@@ -8,6 +8,26 @@ import type { IntermediateResult } from "./types";
 import type * as RDF from "@rdfjs/types";
 import type { JsonValue } from "type-fest";
 
+/**
+ * Represents a placeholder which expects a JSON-LD literal. When a solution is
+ * given binding its variable to an {@link RDF.Literal} which can be represented
+ * as a JSON literal, it will resolve to the literal representation. Note that
+ * not all RDF literals can be represented as JSON literals; if a literal must
+ * be represented in JSON-LD as an object, that object will use an
+ * {@link IRObject} placeholder, since it will take that shape in the query and
+ * the result.
+ *
+ * @example
+ * ### Query
+ * ```json
+ * { "@id": "?" }
+ * ```
+ *
+ * ### Result
+ * ```json
+ * { "@id": "https://swapi.dev/api/people/1/" }
+ * ```
+ */
 export class NativePlaceholder implements IntermediateResult {
   constructor(private readonly variable: RDF.Variable) {}
 
