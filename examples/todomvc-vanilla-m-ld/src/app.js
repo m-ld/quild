86008c6adc13226526e3c62856a0dce76548e45f
Petra Jaros <peeja@peeja.com>
2024-07-26 16:02:16 -0400

    Can set domain from UI

--- examples/todomvc-vanilla-m-ld/src/app.js
+++ examples/todomvc-vanilla-m-ld/src/app.js
@@ -1,6 +1,16 @@
 import { MemoryLevel } from "memory-level";
 import { clone, uuid } from "@m-ld/m-ld";
-import { NullRemotes } from "@m-ld/m-ld/ext/null";
+import { IoRemotes } from "@m-ld/m-ld/ext/socket.io";
+import {
+    BehaviorSubject,
+    fromEvent,
+    map,
+    mergeMap,
+    pairwise,
+    pipe,
+    startWith,
+    tap,
+} from "rxjs";
 
 import View from "./view";
 import Controller from "./controller";
@@ -11,24 +21,67 @@ import "todomvc-app-css/index.css";
 import "todomvc-common/base.css";
 import "./app.css";
 
-/** @type { Todo } */
-let todo;
-const onHashChange = () => {
-    todo.controller.setView(document.location.hash);
-};
-
-const createMeld = () =>
-    clone(new MemoryLevel(), NullRemotes, {
-        "@id": uuid(),
-        "@domain": "m-ld-react.todomvc.com",
-        genesis: true,
-    });
+let currentApp$;
 
 const onLoad = async () => {
-    const storage = await createMeld();
-    todo = new Todo(storage);
-    onHashChange();
+    const domainField = document.getElementById("domain");
+    currentApp$ = new BehaviorSubject(null);
+    const meld$ = fromEvent(domainField, "change").pipe(
+        map((e) => e.target.value),
+        startWith(domainField.value),
+        cloneMap()
+    );
+
+    meld$.pipe(appMap()).subscribe(currentApp$);
 };
+
+/**
+ * Rxjs operator. Maps domains to m-ld instances. Given `""`, it creates a new
+ * genesis domain. Otherwise, it clones the existing domain. Closes each
+ * previous clone as it goes.
+ */
+function cloneMap() {
+    return pipe(
+        map((domain) =>
+            domain.length === 0
+                ? {
+                      "@id": uuid(),
+                      "@domain": `${uuid()}.public.gw.m-ld.org`,
+                      genesis: true,
+                      io: { uri: "https://gw.m-ld.org" },
+                  }
+                : {
+                      "@id": uuid(),
+                      "@domain": domain,
+                      genesis: false,
+                      io: { uri: "https://gw.m-ld.org" },
+                  }
+        ),
+
+        mergeMap((config) => clone(new MemoryLevel(), IoRemotes, config)),
+        startWith(null),
+        pairwise(),
+        map(([prev, next]) => {
+            prev?.close();
+            return next;
+        })
+    );
+}
+
+/**
+ * Rxjs operator. Maps m-ld instances to Todo instances. Disposes of each
+ * previous Todo instance as it goes.
+ */
+function appMap() {
+    return pipe(
+        map((meld) => new Todo(meld)),
+        pairwise(),
+        map(([prev, next]) => {
+            prev.dispose();
+            return next;
+        })
+    );
+}
 
 function Todo(storage) {
     this.storage = storage;
@@ -37,20 +90,27 @@ function Todo(storage) {
     this.view = new View(this.template);
     this.controller = new Controller(this.model, this.view);
 
+    const onHashChange = () => {
+        this.controller.setView(document.location.hash);
+    };
+    window.addEventListener("hashchange", onHashChange);
+    onHashChange();
+
     this.dispose = async () => {
+        // subscription.unsubscribe();
         this.view.dispose();
-        await this.storage.close();
+        window.removeEventListener("hashchange", onHashChange);
     };
 }
 
 /* HOT MODULE SPECIFIC */
 if (module.hot) {
     module.hot.dispose(function () {
-        todo.dispose();
+        currentApp$.value?.dispose();
+        currentApp$.unsubscribe();
     });
     module.hot.accept(function (err) {});
     if (document.readyState === "complete") onLoad();
 }
 
 window.addEventListener("load", onLoad);
-window.addEventListener("hashchange", onHashChange);
